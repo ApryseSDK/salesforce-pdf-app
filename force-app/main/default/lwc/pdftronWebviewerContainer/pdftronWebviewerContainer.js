423fe04232d88363c642803fc069b34271eb28f8
Swoshua <jplore4@gmail.com>
2022-03-16 15:22:04 -0400

    Source Deploy issues

--- force-app/main/default/lwc/pdftronWebviewerContainer/pdftronWebviewerContainer.js
+++ force-app/main/default/lwc/pdftronWebviewerContainer/pdftronWebviewerContainer.js
@@ -3,6 +3,7 @@ import { publish, createMessageContext, releaseMessageContext, subscribe, unsubs
 import WebViewerMC from "@salesforce/messageChannel/WebViewerMessageChannel__c";
 
 export default class PdftronWebviewerContainer extends LightningElement {
+    @api flexipageRegionWidth;
     @api recordId;
     @track renderVideo = false;
     channel;
